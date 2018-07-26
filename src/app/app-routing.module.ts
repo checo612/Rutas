65ce82d0c6c0477c33d12c8fcd62efcfe1fa5c5e
checo612 <40772974+checo612@users.noreply.github.com>
2018-07-26 17:04:20 -0500

    agrege un producto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductosComponent } from './components/productos/productos.component';
+import { Producto1Component } from './components/producto1/producto1.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: '', component: ProductosComponent},
+{path: 'producto1', component: Producto1Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductosComponent } from './components/productos/productos.component';
+import { Producto1Component } from './components/producto1/producto1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductosComponent,
+    Producto1Component
   ],
   imports: [
     BrowserModule,
